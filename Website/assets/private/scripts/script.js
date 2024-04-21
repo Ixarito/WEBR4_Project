5e3085f3b731a150e50e923c0b628a4dda08368b
Marcel Angielczyk <ixarmc@gmail.com>
2024-04-21 20:14:45 +0200

    Home

--- Website/assets/private/scripts/script.js
+++ Website/assets/private/scripts/script.js
@@ -1,6 +1,18 @@
 const hamburger = document.querySelector('.hamburger-menu');
 const navMenu = document.querySelector('.nav-menu');
+
+const title = document.getElementById('title');
+const titleButton = document.getElementById('title-button');
  
 hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('hide');
 });
+
+window.addEventListener('scroll', () => {
+    console.log('scrolling');
+    var scroll = window.scrollY;
+    title.style.marginTop = scroll * 0.7 + 'px';
+    titleButton.style.marginTop = scroll * 0.05 + 'px';
+
+});
+
