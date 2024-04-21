26cf5455cde0139fea18c0cf07d30447efb1f78b
Marcel Angielczyk <ixarmc@gmail.com>
2024-04-21 23:04:57 +0200

    home small fixes

--- Website/assets/private/scripts/script.js
+++ Website/assets/private/scripts/script.js
@@ -1,18 +1,18 @@
 const hamburger = document.querySelector('.hamburger-menu');
-const navMenu = document.querySelector('.nav-menu');
+const navMenu = document.querySelector('.nav-menu-mobile');
 
 const title = document.getElementById('title');
 const titleButton = document.getElementById('title-button');
- 
+
 hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('hide');
+navMenu.classList.toggle('show');
 });
 
 window.addEventListener('scroll', () => {
-    console.log('scrolling');
-    var scroll = window.scrollY;
-    title.style.marginTop = scroll * 0.7 + 'px';
-    titleButton.style.marginTop = scroll * 0.05 + 'px';
+	console.log('scrolling');
+	var scroll = window.scrollY;
+	title.style.marginTop = scroll * 0.6 + 'px';
+	titleButton.style.marginTop = scroll * 0.05 + 'px';
 
 });
 
