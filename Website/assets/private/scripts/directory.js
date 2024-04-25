0bf6cf1047b63cfdaaa6526d3dce36267a6f43d1
Marcel Angielczyk <ixarmc@gmail.com>
2024-04-25 23:09:21 +0200

    park page in progress

--- Website/assets/private/scripts/directory.js
+++ Website/assets/private/scripts/directory.js
@@ -1,27 +1,28 @@
 // include number from 0 (included) to max (excluded)
 function getRandomInt(max) {
-return Math.floor(Math.random() * max);
+	return Math.floor(Math.random() * max);
 }
 
 
 fetch('../assets/private/data/destination.json')
-.then(response => response.json())
-.then(data => {
-	const cardsDisplayArea = document.getElementById('cards-display-area');
-	// simulate several cards
-	for (let i = 0; i < 20; i++) {
-	for (let destination of data) {
-		const cardHTML = `
-		<div class="card">
-			<img class="card-image" src="${destination.parks[getRandomInt(destination.parks.length)].image}">
-			<div class="card-text">
-				<h2 class="card-title">${destination.name}</h2>
-				<p class="card-description">${destination.timezone}</p>
-				<a class="neon-button" href="destination.html?id=${destination.id}">En savoir plus</a>
-			</div>
-		</div>
-		`;
-		cardsDisplayArea.innerHTML += cardHTML;
-	}
-	}
-});
+	.then(response => response.json())
+	.then(data => {
+		const cardsDisplayArea = document.getElementById('destination-display-area');
+		// simulate several cards
+		for (let i = 0; i < 20; i++) {
+
+			const cardHTML = `
+				<div class="card">
+					<img class="card-image" src="${data.parks[getRandomInt(data.parks.length)].image}">
+					<h2 class="card-title">${data.name}</h2>
+					<div class="card-text">
+						<p class="card-description">${data.timezone}</p>
+						<a class="neon-button" href="destination.html?id=${data.id}">En savoir plus</a>
+					</div>
+				</div>
+				`;
+			cardsDisplayArea.innerHTML += cardHTML;
+			
+		}
+	});
+	
