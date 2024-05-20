c6ba1f1be5bba823621dd1ba9d655520083a842c
Marcel Angielczyk <ixarmc@gmail.com>
2024-05-20 17:08:56 +0200

    Laravel - created project

---
+++ website_laravel/website_laravel/resources/views/search.blade.php
@@ -0,0 +1,75 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website</title>
+
+    <!-- navbar css -->
+    <link rel="stylesheet" href="{{asset("css/navbar.css")}}">
+    <!-- global css -->
+    <link rel="stylesheet" href="{{asset("css/global.css")}}">
+    <!-- home css -->
+    <link rel="stylesheet" href="{{asset("css/search.css")}}">
+    <!-- footer css -->
+    <link rel="stylesheet" href="{{asset("css/footer.css")}}">
+    <!-- home css -->
+    <link rel="stylesheet" href="{{asset("css/home.css")}}">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<header>
+    <nav class="navbar">
+        <div class="logo">
+            <a href="index.html">
+                ParkReference
+            </a>
+        </div>
+        <div class="hamburger-menu">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+        </div>
+
+        <!-- desktop -->
+        <div class="nav-menu">
+            <a href="#" class="current-page-menu">Home</a>
+            <a href="#">Career</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+        </div>
+    </nav>
+    <!-- mobile -->
+    <div class="nav-menu-mobile">
+        <a href="#">Home</a>
+        <a href="#">Career</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+    </div>
+</header>
+<div class="page-content">
+    <div class="container">
+        <div class="search-zone">
+            <input class="search-input" type="text" placeholder="Rechercher"><button class="search-button">O</button>
+        </div>
+    </div>
+    <div id="destination-display-area" class="cards-container">
+    </div>
+</div>
+
+<div class="footer">
+    <div class="footer-logo">
+        <a href="/">
+            ParkReference - WERBR4
+        </a>
+    </div>
+    <div class="footer-text">
+        <p>
+            © 2024 Angielczyk Marcel. Tous droits réservés.
+        </p>
+    </div>
+
+</div>
+
+<script src="assets/private/scripts/directory.js"></script>
+<script src="assets/private/scripts/navbar.js"></script>
+</body>
+</html>
